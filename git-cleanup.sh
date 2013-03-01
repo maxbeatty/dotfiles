5b15222ed52ace421745185c55c5886f123cab22
Max Beatty <max@isocket.com>
2013-03-01 15:18:07 -0800

    no color on output fixes cleanup errors

--- git-cleanup.sh
+++ git-cleanup.sh
@@ -6,11 +6,11 @@ git fetch
 git remote prune origin
 
 # Remove local fully merged branches
-git branch --merged master | grep -v 'master$' | xargs git branch -d
+git branch --no-color --merged master | grep -v 'master$' | xargs git branch -d
 
 # Show remote fully merged branches
 echo "The following remote branches are fully merged and will be removed:"
-git branch -r --merged master | sed 's/ *origin\///' | grep -v 'master$'
+git branch --no-color -r --merged master | sed 's/ *origin\///' | grep -v 'master$'
 
 read -p "Continue (y/n)? "
 if [ "$REPLY" == "y" ]
